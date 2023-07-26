6408ee5a9ccbe75d2c96d4e2d54b323ce952a7f8
Ricky Chon <rickychon99@gmail.com>
2023-07-25 23:10:22 -0400

    Add stories for directory and file list items

--- sharemycode-client/src/components/file-explorer/DirectoryListItem/DirectoryListItem.stories.tsx
+++ sharemycode-client/src/components/file-explorer/DirectoryListItem/DirectoryListItem.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { DirectoryListItem } from "../DirectoryListItem";
+import { FaFile, FaFolder } from "react-icons/fa";
+import { PiCaretRightBold } from "react-icons/pi";
 
 const meta: Meta<typeof DirectoryListItem> = {
   component: DirectoryListItem,
@@ -11,5 +13,24 @@ export default meta;
 type Story = StoryObj<typeof DirectoryListItem>;
 
 export const Default: Story = {
-  render: () => <DirectoryListItem />,
+  render: () => <DirectoryListItem label="Default" />,
+};
+
+export const Directory: Story = {
+  render: () => (
+    <DirectoryListItem
+      label="Directory"
+      iconBefore={FaFolder}
+      iconAfter={PiCaretRightBold}
+    />
+  )
+};
+
+export const File: Story = {
+  render: () => (
+    <DirectoryListItem
+      label="My File"
+      iconBefore={FaFile}
+    />
+  )
 };
--- sharemycode-client/src/components/file-explorer/DirectoryListItem/DirectoryListItem.tsx
+++ sharemycode-client/src/components/file-explorer/DirectoryListItem/DirectoryListItem.tsx
@@ -1,26 +1,56 @@
+import React from "react";
 import { IconType } from "react-icons/lib";
 
-export function DirectoryListItem({
-  nodeText = "List Item",
-  iconBefore,
-  iconAfter
-}: {
-  nodeText?: string;
+interface DirectoryListItemProps {
+  /**
+   * Name of the list item
+   */
+  label: string;
+  /**
+   * How long the list item should be
+   */
+  width?: "sm" | "md" | "lg" | "full";
+  /**
+   * Optional icon that displays before label
+   */
   iconBefore?: IconType;
+  /**
+   * Optional icon that displays after label
+   */
   iconAfter?: IconType;
-}) {
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+export function DirectoryListItem({
+  label = "List Item",
+  width = "md",
+  iconBefore,
+  iconAfter,
+}: DirectoryListItemProps): React.ReactNode {
   const IconBefore = iconBefore!;
   const IconAfter = iconAfter!;
 
+  const getWidth = () => {
+    switch (width) {
+      case "sm": return "w-48";
+      case "md": return "w-64";
+      case "lg": return "w-80";
+      case "full": return "w-full";
+    }
+  };
+
   return (
-    <li className="group flex">
+    <li className={`group flex ${getWidth()}`}>
       <button className="flex place-items-center w-full py-1.5 px-2 justify-between hover:bg-zinc-500/20 rounded-md">
         <div className="flex gap-2 place-items-center">
           {iconBefore ? (
             <IconBefore className="w-4 h-4 text-zinc-500 group-hover:text-zinc-200 transition-all duration-100" />
           ) : null}
           <span className="text-zinc-400 text-sm select-none group-hover:text-zinc-100 transition-all duration-100">
-            {nodeText}
+            {label}
           </span>
         </div>
         {iconAfter ? (
